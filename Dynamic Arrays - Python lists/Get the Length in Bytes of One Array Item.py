e96ded9542d496443eb314654c3c22651c149c07
Luka Jasovic <jasovicluka1@gmail.com>
2026-04-07 11:13:23 +0200

    Python Dynamic arrays

---
+++ Dynamic Arrays - Python lists/Get the Length in Bytes of One Array Item.py
@@ -0,0 +1,5 @@
+# Get the Length in Bytes of One Array Item
+# Write a Python program to get the length in bytes of one array item in the internal representation.
+"""Sample Output:
+Original array: array('i', [1, 3, 5, 7, 9])
+Length in bytes of one array item: 4 """
