48e6047f93591adef32df705209e99bd655083c7
Luka Jasovic <jasovicluka1@gmail.com>
2026-05-04 09:31:44 +0200

    finish this

--- MIT algorithms 2020/Sets and sorting/selection_sort_recursive.py
+++ MIT algorithms 2020/Sets and sorting/selection_sort_recursive.py
@@ -1,26 +1,52 @@
 # Initially the same exercise as the "selection_sort" in the same repository, but with using the recursive apporach.
 
-first_list = [8,2,4,9,3,16,255]
-L = len(first_list)
-# i will define find max function that i will call recursively during this task
+first_list = [8,2,4,9,3,67,16]
 
-# find me the first 
-z = [41]
+# Split the problem in smaller parts first:
 
-def find_max_id(arr):
+# 1. Define the Base Case
+# The recursion stops when the list has only one element. 
+# In this case, the only available index is \(0\), which is naturally the index of the maximum element
 
-    
+
+# 2. Recursive Step and Comparison
+# If the list has more than one element,
+# recursively find the index of the maximum value in the rest of the list (from index \(1\) to \(n-1\)).
+
+def get_max_id(arr):
+
     if len(arr) == 1:
         return 0
+    
+    rest_array = arr[1:]
 
-    next_id = find_max_id(arr[1:]) + 1
+    rest_array_max = get_max_id(rest_array)
 
-    if arr[0] > arr[next_id]:
+    if arr[0] > rest_array[rest_array_max]:
         return 0
     else:
-        return next_id
+        return rest_array_max + 1
+    
 
-z = find_max_id(first_list)
+L = len(first_list)
 
-print(f"Index of maximum number in this list is: {[z]}\nNumber: {first_list[z]}\n{'*'*15}")
+# get max id within the full list n
+# compare the max with the max of the sublist n -1
+# swap
 
+def selection_sort(arr,L):
+    
+    if L == 1:
+        return arr[0]
+    
+    max_id = get_max_id(arr)
+    
+    arr[max_id],arr[-1] = arr[-1],arr[max_id]
+    
+
+
+
+
+
+z = selection_sort(first_list,L)
+print(z)
