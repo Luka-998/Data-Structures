7b9950c8a7207bb2ad254679fa0cbf0026fd1fa2
Luka Jasovic <jasovicluka1@gmail.com>
2026-05-13 08:02:28 +0200

    pay attention to this one

--- Dynamic Arrays - Python lists/RemoveDuplicatesFromSorted.py
+++ Dynamic Arrays - Python lists/RemoveDuplicatesFromSorted.py
@@ -9,22 +9,25 @@ Input: arr[] = [2, 2, 2, 2, 2]
 Output: [2]
 Explanation: All the elements are 2, So only keep one instance of 2.
 
-space O(n) solution first
+space O(1) solution
 
 """
 
-array =  [2, 2, 2, 2]
+array = [2, 2, 2, 2, 3, 4]
 
 def remove_duplicates(arr):
-    #base case
+    if len(arr) == 0:
+        return 0
+    
+    write_index = 1
 
-    seen = False
-    new_list = []
-    for i in range(len(arr)):
-        if arr[i] not in new_list and arr[i] is not seen:
-            new_list.append(arr[i])
-            seen = True
-    return new_list
+    for i in range(1, len(arr)):
+        if arr[i] != arr[write_index - 1]:
+            arr[write_index] = arr[i]
+            write_index += 1
+
+    return write_index
 
 z = remove_duplicates(array)
 print(z)
+print(array[:z])
