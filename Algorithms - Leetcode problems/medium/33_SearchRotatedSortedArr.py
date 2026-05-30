58b8d4d8aaa5801439d0994c343fc8399670d608
Luka Jasovic <jasovicluka1@gmail.com>
2026-05-30 13:53:21 +0200

    --amend

--- Algorithms - Leetcode problems/medium/33_SearchRotatedSortedArr.py
+++ Algorithms - Leetcode problems/medium/33_SearchRotatedSortedArr.py
@@ -41,18 +41,26 @@ def get_search(nums,target):
     left = 0
     right = len(nums) - 1
     
-    while left < right:
+    while left <= right:
         mid = (left+right) // 2 # 3
         
         if nums[mid] == target:
             return mid
-        
-        elif nums[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
 
+        # check if left is sorted
+
+        if nums[left] <= nums[mid]: #sorted
+            if nums[left] <= target < nums[mid]: # go left
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        elif nums[mid] <= nums[right]:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid -1
+    return -1
 
 z = get_search(nums,target)
 print(z)
