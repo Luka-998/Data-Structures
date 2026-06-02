468319d13ed2c1c41dccbd4000d1004ac59d7654
Luka Jasovic <jasovicluka1@gmail.com>
2026-06-02 12:06:53 +0200

    done time O(n^2)

---
+++ Algorithms - Leetcode problems/easy/2_TwoSum.py
@@ -0,0 +1,50 @@
+# 1. Two Sum
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+-------
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+"""
+nums = [3,2,3]
+target = 6
+
+def get_target(arr,target):
+
+    if len(arr) == 2:
+        return [0,1]
+    
+    for i in range(len(arr)):
+        curr = 0
+        for j in range(i,len(arr)):
+            if i == j:
+                continue
+            curr = arr[i] + arr[j]
+            if curr == target:
+                return [i,j]
+            else:
+                curr = 0
+
+                
+z = get_target(nums,target)
+print(z)
