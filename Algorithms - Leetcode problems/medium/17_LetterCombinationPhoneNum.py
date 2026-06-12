85bf37df09b0e63802d9afd879a118b4cf989c41
skeja <jasovicluka1@gmail.com>
2026-06-12 22:21:12 +0200

    heavy lifting

---
+++ Algorithms - Leetcode problems/medium/17_LetterCombinationPhoneNum.py
@@ -0,0 +1,62 @@
+# 17. Letter Combinations of a Phone Number
+
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+
+Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+
+ 
+
+Constraints:
+
+    1 <= digits.length <= 4
+    digits[i] is a digit in the range ['2', '9'].
+
+"""
+
+mapps = {'2':['a','b','c'],'3':['d','e','f'],'4':['g','h','i'],'5':['j','k','l'],'6':['m','n','o'],'7':['p','q','r','s'],'8':['t','u','v'],'9':['w','x','y','z']}
+
+digits = "234" # len = 2
+
+def get_letters(digits):
+
+    i = 0 
+
+    res = []
+    final = []
+    current = []
+
+    if len(digits) == 1:
+        return mapps[digits[0]]
+    else:
+        while i < len(digits):
+            if res == []: # prva iteracija 
+                res.extend(mapps[digits[i]])
+              #  print(res)
+                i+=1
+            else:
+                for r in res:
+                    for curr in mapps[digits[i]]:
+                        current.append(r+curr)
+                res = current
+                current = []
+                i+=1
+        return res
+
+        
+
+z = get_letters(digits)
+print(z)
+
