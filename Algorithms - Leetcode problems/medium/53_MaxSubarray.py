f834c2fbd28c813f2297ca237200fedceb52ed96
Luka Jasovic <jasovicluka1@gmail.com>
2026-07-27 13:08:56 +0200

    Logic solved , leetcode needs kadane algorithm to pass

---
+++ Algorithms - Leetcode problems/medium/53_MaxSubarray.py
@@ -0,0 +1,51 @@
+"""
+Medium:
+
+Given an integer array nums, find the with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+
+
+"""
+
+num = [5,4,-1,7,8]
+# subarray
+
+def solution(arr):
+
+    total_sum = -99
+
+    for i in range(len(arr)):
+        current_sum = 0
+        for j in range(i,len(arr)):
+            current_sum+=arr[j]
+            if current_sum > total_sum:
+                total_sum = current_sum
+    return total_sum
+
+z = solution(num)
+print(z)
+
