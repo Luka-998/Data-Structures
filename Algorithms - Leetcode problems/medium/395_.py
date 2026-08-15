72e082ac8562b03d9ebb5fe88b3f34e7536c80c7
Luka Jasovic <jasovicluka1@gmail.com>
2026-08-15 14:39:59 +0200

    accepted brute force

--- Algorithms - Leetcode problems/medium/395_.py
+++ Algorithms - Leetcode problems/medium/395_.py
@@ -19,39 +19,42 @@ Output: 5
 Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
 
 """
+from timeit import default_timer 
 
-s = "ababbc"
-target = 2
+s = "aaabb"
+target = 3
+start = default_timer()
 
 def solution(arr,target):
-
-    res = 0
-
-    for i in range(len(arr)):
+    
+    res = 0     
+    
+    for i in range(len(s)):
         current = ''
         tracker = {}
-        for j in range(i,len(arr)):
-            current +=arr[j] # a , b {a:1, b:1}
+        for j in range(i,len(s)):
+            is_valid = True
+            current+=arr[j]
             if arr[j] not in tracker:
                 tracker[arr[j]] = 1
             else:
-                tracker[arr[j]]+=1
+                tracker[arr[j]] +=1
             for key,value in tracker.items():
-                if tracker[key] < target:
-                    continue
-                elif tracker[key] >= target:
-                    print(current[key])
+                if value < target:
+                    is_valid = False
+                    break
+            if is_valid:
+                if len(current) > res:
+                    res = len(current)
 
-            
-                    
-
-
-                    
-
-
-                
-
-        
+    if res ==0:
+        end = default_timer()
+        print(start-end)
+        return 0
+    else:
+        end = default_timer()
+        print(start-end)
+        return res
 
 z = solution(s,target)
 print(z)
